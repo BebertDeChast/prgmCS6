6fb9ad65c537a6dc6ceac5b39efe67d3264df3d6
BebertDeChast <hdechastellux@gmail.com>
2022-01-17 13:32:17 +0100

    1332

---
+++ rubish/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PI 3.14
+#define square(x) (x)*(x)
+
+
+void printHello(){
+    printf("Hello world!\n");
+}
+
+int main(int argc, char *argv[]){
+    printHello();
+    return 0;
+}
+
--- test.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#define PI 3.14
-#define square(x) (x)*(x)
-
-int main(){
-    return 0;
-}
